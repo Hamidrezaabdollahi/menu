84572f4347705750e92e4c33e3e84cfdcb228d57
hamidreza <hamid.r.abdollahi79@gmail.com>
2025-05-11 15:01:38 +0330

    inhance flat menu

--- src/components/FlatMenu.tsx
+++ src/components/FlatMenu.tsx
@@ -19,58 +19,42 @@ const FlatMenu = () => {
     dataFetcher();
   }, []);
 
+  const renderMenuItem = (item: FlatMenu, depth: number = 0) => {
+    const children = menu.filter((child) => child.parentId === item.id);
+
+    return (
+      <div key={item.id} className="w-full h-full flex flex-col gap-4">
+        <div className="w-full h-full flex items-center justify-center">
+          <span
+            className={`${
+              depth === 0
+                ? "bg-gray-400 text-white"
+                : "bg-gray-200 text-gray-500"
+            } w-full h-full`}
+            style={{ marginRight: `${depth * 16}px` }}
+          >
+            {item.name}
+          </span>
+        </div>
+        {children.length > 0 && (
+          <div className="ml-4">
+            {children.map((child) => renderMenuItem(child, depth + 1))}
+          </div>
+        )}
+      </div>
+    );
+  };
+
+
 
   
-
   return (
-    <div className="w-full relative h-full ">
+    <div className="w-full relative h-full">
       {menu
         .filter((item) => item.parentId === null)
-        .map((parent) => {
-          const children = menu.filter((item) => item.parentId === parent.id);
-          return (
-            <div key={parent.id} className="w-full h-full flex flex-col gap-4">
-              <div className="w-full h-full flex items-center justify-center">
-                <span className="bg-gray-400 text-white w-full h-full text-center">
-                  {parent.name}
-                </span>
-              </div>
-           
-              {children.map((child) => {
-                const grandchildren = menu.filter(
-                  (item) => item.parentId === child.id
-                );
-                return (
-                  <div>
-                    <div
-                      key={child.id}
-                      className="w-full h-full flex items-center justify-center text-sm"
-                    >
-                      <span>{child.name}</span>
-                    </div>
-                    {grandchildren.map((grandchild) => {
-                      return (
-                        <div
-                          key={grandchild.id}
-                          className="w-full h-full flex items-center justify-center text-xs"
-                        >
-                          <span className="text-red-600">{grandchild.name}</span>
-                        </div>
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+        .map((item) => renderMenuItem(item))}
     </div>
   );
-}
+};
 
 export default FlatMenu;
-
-
-function SubMenu(){
-    
-}
